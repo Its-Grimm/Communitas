36085d80e3997317d360afdf59d0fa2cb6185314
Tyler Patenaude <witwityler@gmail.com>
2026-05-16 23:23:22 -0700

    Added file saving, getting latest file, delete/edit/view local entry, and a bunch more.

--- cmm.sh
+++ cmm.sh
@@ -4,42 +4,25 @@ show_help(){
    echo "Showing help!"
 }
 
+# Directory structure: ./Journal/Year/Month/Journal entries sorted by date then time
+save_file(){
+   YEAR="$(date '+%Y')"
+   MONTH="$(date '+%B')"
+   DATE="$(date '+%F')"
+   TIME="$(date '+%R')"
+
+   DIR="./Journal/$YEAR/$MONTH"
+   mkdir -p "$DIR"
+
+   local USER_CONTENT=$1
+   echo "$USER_CONTENT" > "./Journal/$YEAR/$MONTH/$DATE-$TIME.md"
+}
+
 write_entry(){
    local tmpfile
    local vimrc
    tmpfile="$(mktemp)"
-   vimrc="$(mktemp)"
-
-   cat > "$vimrc" << EOF
-set nocompatible
-set noswapfile
-set nobackup
-set nowritebackup
-syntax on
-set termguicolors
-highlight Normal guifg=#f2f4f8 guibg=NONE                               " Default text
-highlight LineNr guifg=#525253 guibg=NONE                               " Gutter / line numbers
-highlight Keyword guifg=#8D5DC7 gui=bold guibg=NONE             " Keywords
-highlight CursorLineNr guifg=#f2f4f8 gui=bold guibg=NONE " Current line number (optional, makes it stand out)
-highlight Comment guifg=#525253 gui=bold guibg=NONE
-
-set autoindent
-set tabstop=3
-set shiftwidth=3
-set relativenumber
-
-" live character counter
-function! CharCount()
-   let l:count = strlen(join(getline(1, '$'), "\n"))
-   return "Chars: " . l:count . " / 500"
-endfunction
-
-set statusline=%f\ %=%{CharCount()}
-
-set laststatus=2
-EOF
-
-   vim -u "$vimrc" "$tmpfile"
+   vim -u ./vimrc "$tmpfile"
    USER_INPUT="$(cat "$tmpfile")"
 
    if (( ${#USER_INPUT} > 500 )); then
@@ -47,51 +30,115 @@ EOF
       exit 1
    fi
 
+   if (( ${#USER_INPUT} < 2 )); then
+      echo "File discarded"
+      exit 1
+   fi
    # echo "File contents:"
    # echo "$(cat "$tmpfile")"
 
    rm -f "$tmpfile"
+
+   save_file "$USER_INPUT"
 }
 
 post_entry(){
-   # TODO: Image upload features
+   # TODO: Image upload feature
+   # Maybe handled by sending images to an image storage site, then linking the image into the entry/post
    set -e
    set -a
    source .env
    set +a
 
    # TODO: Allow multiple API's compatable and configurable through .config file
-   # Mastodon for now
+   # Default Mastodon for now
    # curl -s -X POST "$INSTANCE/api/v1/statuses" \
    #    -H "Authorization: Bearer $API_KEY" \
    #    -d "status=$USER_INPUT"
 
-   echo "Posted entry:"
-   echo "$USER_INPUT"
+   # echo "Posted entry:"
+   # echo "$USER_INPUT"
+}
+
+get_latest(){
+   LATEST_FILE="$(find ./Journal -type f -name "*.md" | sort | tail -n 1)"
+   # echo "$LATEST_FILE"
+   # echo "$(cat $LATEST_FILE)"
+   echo "$LATEST_FILE"
 }
 
 case "$1" in 
    -w | --write)
       write_entry
-      ;;
+   ;;
 
    -p | --post)
       post_entry
-      ;;
+   ;;
 
    -wp | --write-post)
       write_entry
       post_entry
-      ;;
+   ;;
 
    -d | --delete)
+      case "$2" in 
+         latest | l)
+            LATEST_FILE=$(get_latest)
+            rm "$LATEST_FILE"
+         ;;
+         *)
+            # TODO: Filter garbage out (only allow format specified in config)
+            # Then allow deletion of certain saved file
+         ;;
+      esac
+   ;;
+   
+   -dp | --delete-post)
+      case "$2" in
+         latest | l)
+         ;;
+         *)
+         ;; 
+      esac
+   ;;
 
-      ;;
+   -e | --edit)
+      case "$2" in
+         latest | l)
+            LATEST_FILE=$(get_latest)
+            vim -u ./vimrc "$LATEST_FILE"
+         ;;
+         *)
+            # TODO: Filter garbage out (only allow format specified in config)
+            # Then allow deletion of certain saved file
+         ;;
+      esac
+   ;;
 
-   -h | --help) 
-      show_help
-      ;;
-   *)
-      show_help
-      ;;
+   -v | --view)
+      case "$2" in
+         latest | l)
+            LATEST_FILE=$(get_latest)
+            cat "$LATEST_FILE"
+         ;;
+         *)
+
+         ;;
+      esac
+   ;;
+
+   -vp | --view-post)
+      case "$2" in
+         latest | l)
+            LATEST_FILE=$(get_latest)
+         ;;
+         *)
+
+         ;;
+      esac
+   ;;
+
+   -h | --help) show_help ;;
+   *)           show_help ;;
 esac
